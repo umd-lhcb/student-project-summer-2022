a8088a0c7d92aaa1048bd319fd5cb0cd9500d815
Yipeng Sun <syp@umd.edu>
2022-06-27 16:28:00 -0400

    Setup a jupyter lab env.

--- flake.nix
+++ flake.nix
@@ -2,49 +2,33 @@
   description = "Project for 2022 summer high school students.";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
+    jupyterWith.url = "github:tweag/jupyterWith";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, jupyterWith, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           config = { allowUnfree = true; };
-          # overlays = [];
+          overlays = nixpkgs.lib.attrValues jupyterWith.overlays;
         };
-        python = pkgs.python3;
-        pythonPackages = python.pkgs;
-      in
+        iPython = pkgs.kernels.iPythonWith {
+          name = "Python-env";
+          packages = p: with p; [ numpy matplotlib uproot ];
+        };
+        jupyterEnvironment = pkgs.jupyterlabWith {
+          kernels = [ iPython ];
+        };
+      in rec
       {
-        devShell = pkgs.mkShell {
-          name = "student-project-summer-2022";
-          buildInputs = with pythonPackages; [
-            pkgs.root
-
-            # Python requirements (pin as much as possible)
-            numpy
-            matplotlib
-            uproot
-            jupyter
-          ];
-
-          FONTCONFIG_FILE = pkgs.makeFontsConf {
-            fontDirectories = with pkgs; [
-              gyre-fonts
-            ];
-          };
-
-          shellHook = ''
-            # Filter out tensorflow and zfit warnings
-            export TF_CPP_MIN_LOG_LEVEL=2
-            export ZFIT_DISABLE_TF_WARNINGS=1
-
-            # matplotlib gloabl config
-            export MPLBACKEND=agg  # the backend w/o a UI
-            export MPLCONFIGDIR=$(pwd)/.matplotlib
-          '';
+        apps.jupyterlab = {
+          type = "app";
+          program = "${jupyterEnvironment}/bin/jupyter-lab";
         };
+        defaultApp = apps.jupyterlab;
+        devShell = jupyterEnvironment.env;
       });
 }
